9870540f5111d663bb02c5ce979e5e1bd6b5c2f3
Katarzyna Michalowska <katarzyna.michalowska@sintef.no>
2025-02-19 17:31:44 +0100

    Adding plotting individual instances

--- test_model.py
+++ test_model.py
@@ -12,7 +12,8 @@ from modules import plot
 @hydra.main(version_base=None, config_path="configs", config_name="config_test")
 def test_model(cfg: DictConfig):
     model_path = cfg.model_path
-    os.environ["HYDRA_RUN_DIR"] = os.path.join(model_path, "test")    
+    output_folder = os.path.join(model_path, "test")
+    os.environ["HYDRA_RUN_DIR"] = output_folder    
     logging.basicConfig(level=logging.INFO)
     
     device = cfg.device
@@ -24,6 +25,10 @@ def test_model(cfg: DictConfig):
         data["X_train_scaled"], data["X_test_scaled"], data["y_train_scaled"], data["y_test_scaled"], data["y_scaler"]
     )
 
+    X_train_scaled = X_train_scaled.float().to(device)
+    X_test_scaled = X_test_scaled.float().to(device)
+    y_train_scaled = y_train_scaled.float().to(device)
+    y_test_scaled = y_test_scaled.float().to(device)
 
     model = LSTM(X_train_scaled.shape[2], cfg_model.model.hidden_size, y_train_scaled.shape[2])
     
@@ -51,7 +56,9 @@ def test_model(cfg: DictConfig):
 
             logging.info(f"CP {cp} \t Train Loss: {train_loss:.4f} \t Test Loss: {test_loss:.4f}")
 
-    plot.plot_losses(train_losses, test_losses, epochs, loss_name, os.path.join(model_path, "test"))
+        plot.plot_predictions(y_test_scaled.detach().cpu(), y_test_pred.detach().cpu(), i0=0, i1=1000, t_len=y_test_scaled.shape[1], save_path=os.path.join(output_folder, f"ts_{cp}.pdf"))
+
+    plot.plot_losses(train_losses, test_losses, epochs, loss_name, save_path=os.path.join(output_folder, f"loss_plot_{loss_name}.pdf"))
 
   
 if __name__ == "__main__":
