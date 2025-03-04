b0ceb4e4f06a1b944e723d82c27917d8996dfb8e
Katarzyna Michalowska <katarzyna.michalowska@sintef.no>
2025-03-04 02:06:48 +0100

    Added weight_decay parameter and AdamW optimizer

--- modules/optimizers.py
+++ modules/optimizers.py
@@ -1,16 +1,19 @@
 import torch.optim as optim
+#from adabelief_pytorch import AdaBelief
 
-def get_optimizer(optimizer_name, model, lr, eps=1e-8, betas=(0.9, 0.999)):
+
+def get_optimizer(optimizer_name, model, lr, eps=1e-8, betas=(0.9, 0.999), weight_decay=1e-4):
     if optimizer_name == 'adam':
-        return optim.Adam(model.parameters(), lr=lr, eps=eps, betas=betas)
+        return optim.Adam(model.parameters(), lr=lr, eps=eps, betas=betas, weight_decay=weight_decay)
+    elif optimizer_name == 'adamw':  # Added AdamW
+        return optim.AdamW(model.parameters(), lr=lr, eps=eps, betas=betas, weight_decay=weight_decay)
     elif optimizer_name == 'sgd':
-        return optim.SGD(model.parameters(), lr=lr)
-    elif optimizer_name == 'adabelief':
-        from adabelief_pytorch import AdaBelief
-        return AdaBelief(model.parameters(), lr=lr, eps=eps, betas=betas)
+        return optim.SGD(model.parameters(), lr=lr, weight_decay=weight_decay)
+    #elif optimizer_name == 'adabelief':
+    #    return AdaBelief(model.parameters(), lr=lr, eps=eps, betas=betas, weight_decay=weight_decay)
     elif optimizer_name == 'adagrad':
-        return optim.Adagrad(model.parameters(), lr=lr)
+        return optim.Adagrad(model.parameters(), lr=lr, weight_decay=weight_decay)
     elif optimizer_name == 'rmsprop':
-        return optim.RMSprop(model.parameters(), lr=lr, eps=eps, betas=betas)
+        return optim.RMSprop(model.parameters(), lr=lr, eps=eps, momentum=betas[0], weight_decay=weight_decay)
     else:
         raise ValueError(f"Optimizer {optimizer_name} not recognized")
